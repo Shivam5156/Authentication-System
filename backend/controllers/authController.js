00f105ad1090a3bd10dd914231deba5910976e88
shivam singh <singhs36533@gmail.com>
2026-07-01 10:10:41 +0530

    bug fixes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -107,10 +107,9 @@ const login = async (req, res) => {
         //cookie parser
         res.cookie('token', token, {
             httpOnly: true,
-            secure:  false,
-            sameSite: 'lax',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
-
         });
 
         return res.status(200).json({
